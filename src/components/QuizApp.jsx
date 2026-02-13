bafa1c3c66796c5ae9c13e67c58bd50bfb3b9b42
Maxim Murysov <murisov98@mail.ru>
2026-02-13 13:22:29 +0300

    add number of quastions and style

--- src/components/QuizApp.jsx
+++ src/components/QuizApp.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
+import { questions } from "./questions";
 
 function QuizApp() {
-  return <div>fff</div>;
+  return (
+    <div className={styles.quiz}>
+      <div className={styles.quizContainer}>
+        <div className={styles.numberQuestion}>
+          {questions.map((_, i) => (
+            <button key={i} className={styles.questionItem}>
+              {i + 1}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 export default QuizApp;
---
+++ src/components/questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+  {
+    title: "What is the capital of Canada?",
+    list: [
+      { answer: "Ottawa", term: true },
+      { answer: "Vancouver", term: false },
+      { answer: "Melbourne", term: false },
+      { answer: "Chicoutimi", term: false },
+    ],
+  },
+  {
+    title: "How many people live in Russia?",
+    list: [
+      { answer: "117 million", term: false },
+      { answer: "162 million", term: false },
+      { answer: "155 million", term: false },
+      { answer: "144 million", term: true },
+    ],
+  },
+  {
+    title: "What is the distance from Moscow to Vladivostok?",
+    list: [
+      { answer: "2000 km", term: false },
+      { answer: "3500 km", term: false },
+      { answer: "6000 km", term: false },
+      { answer: "9000 km", term: true },
+    ],
+  },
+  {
+    title: "Which country has the largest territory in the world?",
+    list: [
+      { answer: "Canada", term: false },
+      { answer: "Russia", term: true },
+      { answer: "China", term: false },
+      { answer: "USA", term: false },
+    ],
+  },
+];
